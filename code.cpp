7671b1628779c3d5089163e040ea0a9731e21d6a
Diksha Nasa <64835443+dikshanasa@users.noreply.github.com>
2022-01-26 12:36:58 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,9 +1,16 @@
 void swapAlternate(int *arr, int size)
 {
-    for(int i=0;i<=size;i+=2){
-        int temp= arr[i];
-        arr[i]=arr[i+1];
-        arr[i+1]=temp;
+    int i;
+      for (i = 0; i < size; i += 2)
+    {
+       
+        if (i + 1 == size)
+        {
+            break;
+        }
+
+       int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
     }
-    //Write your code here
 }
